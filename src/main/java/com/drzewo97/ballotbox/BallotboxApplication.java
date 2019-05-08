db85c04c6520271429acdccbe15462d8b5376f8e
Unknown <wojciech97.drzewiecki@gmail.com>
2019-05-08 22:42:14 +0200

    Initializr commit

---
+++ src/main/java/com/drzewo97/ballotbox/BallotboxApplication.java
@@ -0,0 +1,13 @@
+package com.drzewo97.ballotbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallotboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallotboxApplication.class, args);
+	}
+
+}
