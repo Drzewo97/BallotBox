f82ec1a0e3b4ea3f213b41be802d3a0e2c744d27
Unknown <wojciech97.drzewiecki@gmail.com>
2019-05-19 00:01:46 +0200

    Add CollectionSize validator annotation

---
+++ src/main/java/com/drzewo97/ballotbox/constraint/collectionsize/CollectionSizeValidator.java
@@ -0,0 +1,56 @@
+package com.drzewo97.ballotbox.constraint.collectionsize;
+
+import org.springframework.beans.BeanWrapperImpl;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.beans.PropertyDescriptor;
+import java.util.Collection;
+
+/**
+ * Validator for CollectionSize annotation
+ * @see CollectionSize
+ */
+public class CollectionSizeValidator implements ConstraintValidator<CollectionSize, Object> {
+
+    private String sizeFieldName;
+    private String collectionFieldName;
+    private String message;
+
+    @Override
+    public void initialize(final CollectionSize constraintAnnotation) {
+        sizeFieldName = constraintAnnotation.size();
+        collectionFieldName = constraintAnnotation.collection();
+        message = constraintAnnotation.message();
+    }
+
+    @Override
+    public boolean isValid(final Object value, final ConstraintValidatorContext context) {
+        boolean valid = false;
+        try
+        {
+            BeanWrapperImpl wrapper = new BeanWrapperImpl(value);
+            final Object sizeObj = wrapper.getPropertyDescriptor(sizeFieldName);
+            final Object collectionObj = wrapper.getPropertyDescriptor(collectionFieldName);
+
+            Integer size = (Integer)((PropertyDescriptor) sizeObj).getReadMethod().invoke(value);
+            Collection<?> collection = (Collection<?>)((PropertyDescriptor) collectionObj).getReadMethod().invoke(value);
+
+            valid = size <= collection.size();
+        }
+        catch (final Exception ignore)
+        {
+            // ignore
+            //TODO: wrong cast
+        }
+
+        if (!valid){
+            context.buildConstraintViolationWithTemplate(message)
+                    .addPropertyNode(collectionFieldName)
+                    .addConstraintViolation()
+                    .disableDefaultConstraintViolation();
+        }
+
+        return valid;
+    }
+}
