da38365e2dde123a575d8806d2ae6a7c03a76eea
Unknown <wojciech97.drzewiecki@gmail.com>
2019-05-19 16:00:00 +0200

    Add support of sec:* attributes in thymeleaf

---
+++ src/main/java/com/drzewo97/ballotbox/config/ThymeleafConfig.java
@@ -0,0 +1,21 @@
+package com.drzewo97.ballotbox.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver, SpringSecurityDialect sec) {
+        final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        // Add support of sec:* attributes
+        templateEngine.addDialect(sec);
+        return templateEngine;
+    }
+}
