643fd767133265aead6f2c1786c3f926ffc5d312
Wojciech Drzewiecki <wojciech.drzewiecki@sap.com>
2019-11-08 09:36:53 +0100

    Add panel page

--- src/main/java/com/drzewo97/ballotbox/config/SecurityConfiguration.java
+++ src/main/java/com/drzewo97/ballotbox/config/SecurityConfiguration.java
@@ -26,7 +26,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception{
         http.authorizeRequests()
-                .antMatchers("/h2_console/**", "/h2/**", "/manage/users/**").hasRole("ADMIN")
+                .antMatchers("/h2_console/**", "/h2/**", "/panel/**").hasRole("ADMIN")
                 .antMatchers("/manage/**").hasRole("MODERATOR")
                 .antMatchers("/registration").permitAll()
                 .antMatchers("/**").hasRole("USER")
