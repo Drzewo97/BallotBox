f27a1afd415ab07abd647329cec38a9b8e89254c
Wojciech Drzewiecki <wojciech.drzewiecki@sap.com>
2020-01-13 22:25:06 +0100

    Mark poll as voted by user

--- src/main/java/com/drzewo97/ballotbox/votingmachine/controller/election/ElectionVoteController.java
+++ src/main/java/com/drzewo97/ballotbox/votingmachine/controller/election/ElectionVoteController.java
@@ -6,11 +6,14 @@ import com.drzewo97.ballotbox.core.model.candidate.CandidateRepository;
 import com.drzewo97.ballotbox.core.model.election.Election;
 import com.drzewo97.ballotbox.core.model.election.ElectionRepository;
 import com.drzewo97.ballotbox.core.model.poll.Poll;
+import com.drzewo97.ballotbox.core.model.user.User;
+import com.drzewo97.ballotbox.core.model.user.UserRepository;
 import com.drzewo97.ballotbox.core.model.vote.Vote;
 import com.drzewo97.ballotbox.core.model.vote.VoteRepository;
 import com.drzewo97.ballotbox.core.service.converter.Converter;
 import com.drzewo97.ballotbox.votingmachine.dto.VoteDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -36,6 +39,9 @@ public class ElectionVoteController {
 	@Autowired
 	private Converter<Set<Map.Entry<Candidate, VoteDto>>, Vote> converter;
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@GetMapping
 	public String showElections(@PathVariable("id") Integer id, WebRequest request, Model model) {
 		Optional<Election> election = electionRepository.findById(id);
@@ -51,7 +57,11 @@ public class ElectionVoteController {
 	}
 	
 	@PostMapping
-	public String registerVote(@ModelAttribute("electionVote") ElectionVoteDto electionVoteDto, BindingResult result){
+	public String registerVote(@PathVariable("id") Integer id, @ModelAttribute("electionVote") ElectionVoteDto electionVoteDto, BindingResult result){
+		Optional<Election> election = electionRepository.findById(id);
+		if(election.isEmpty()){
+			return "redirect:/election/all?empty";
+		}
 		if(result.hasErrors()){
 			return "panel/district_create";
 		}
@@ -62,6 +72,13 @@ public class ElectionVoteController {
 		for(Set<Map.Entry<Candidate, VoteDto>> candidates : pollCandidates.values()){
 			voteRepository.save(converter.convert(candidates));
 		}
+		
+		String currentPrincipalName = SecurityContextHolder.getContext().getAuthentication().getName();
+		User user = userRepository.findByUsername(currentPrincipalName).get();
+		for(Poll poll:election.get().getPolls()){
+			user.getPollsVoted().add(poll);
+		}
+		userRepository.save(user);
 		
 		return "redirect:/election/all?success";
 	}
