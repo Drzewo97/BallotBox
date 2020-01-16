8dbaf143a2b35db067251d2a4df49af2850c7ce2
Wojciech Drzewiecki <wojciech.drzewiecki@sap.com>
2020-01-16 16:12:26 +0100

    Add committee candidates order validation

--- src/main/java/com/drzewo97/ballotbox/committeepanel/controller/candidates/ManageCandidatesOrderController.java
+++ src/main/java/com/drzewo97/ballotbox/committeepanel/controller/candidates/ManageCandidatesOrderController.java
@@ -10,8 +10,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 @Controller
 @RequestMapping("/committeepanel/{committeeId}/order")
@@ -26,15 +29,30 @@ public class ManageCandidatesOrderController {
 	@Autowired
 	private CommitteeCandidateOrderRepository committeeCandidateOrderRepository;
 	
+	private final String POST_ORDERING_LANDING_PAGE = "redirect:/committeepanel/";
+	
 	@GetMapping
-	public String showManageCandidatesOrder(Model model, @PathVariable("committeeId") Integer committeeId){
+	public String showManageCandidatesOrder(Model model,
+	                                        @PathVariable("committeeId") Integer committeeId,
+	                                        RedirectAttributes redirectAttributes)
+	{
+		List<String> outputMessages = new ArrayList<>();
+		redirectAttributes.addFlashAttribute("messages", outputMessages);
+		
 		//Get username
 		String currentPrincipalName = SecurityContextHolder.getContext().getAuthentication().getName();
 		if(!committeeService.isCommitteeAdmin(currentPrincipalName, committeeId)){
-			return "redirect:/";
+			outputMessages.add("You are not this committee admin!");
+			return POST_ORDERING_LANDING_PAGE + committeeId;
 		}
 		
 		Committee committee = committeeRepository.findById(committeeId).get();
+		
+		if(committee.getMembers().size() == 0){
+			// no members to order
+			outputMessages.add("There are no members!");
+			return POST_ORDERING_LANDING_PAGE + committeeId;
+		}
 		
 		CandidatesOrderDto candidatesOrderDto = new CandidatesOrderDto();
 		candidatesOrderDto.setCandidatesOrder(new ArrayList<>(committee.getCandidateOrders()));
@@ -46,17 +64,49 @@ public class ManageCandidatesOrderController {
 	}
 	
 	@PostMapping
-	public String manageCandidatesOrder(@ModelAttribute("candidatesOrderDto") CandidatesOrderDto candidateOrders, @PathVariable("committeeId") Integer committeeId){
+	public String manageCandidatesOrder(@ModelAttribute("candidatesOrderDto") CandidatesOrderDto candidateOrders,
+	                                    @PathVariable("committeeId") Integer committeeId,
+	                                    RedirectAttributes redirectAttributes)
+	{
+		List<String> outputMessages = new ArrayList<>();
+		redirectAttributes.addFlashAttribute("messages", outputMessages);
+		
 		//Get username
 		String currentPrincipalName = SecurityContextHolder.getContext().getAuthentication().getName();
 		if(!committeeService.isCommitteeAdmin(currentPrincipalName, committeeId)){
-			return "redirect:/";
+			outputMessages.add("You are not this committee admin!");
+			return POST_ORDERING_LANDING_PAGE + committeeId;
 		}
 		
-		//TODO: validity check
+		Committee committee = committeeRepository.findById(committeeId).get();
+		
+		if(committee.getMembers().size() == 0){
+			// no members to order
+			outputMessages.add("There are no members!");
+			return POST_ORDERING_LANDING_PAGE + committeeId;
+		}
+		
+		// check for null/0/negative values
+		if(candidateOrders.getCandidatesOrder().stream().anyMatch(o -> o.getCandidateOrder() == null || o.getCandidateOrder() < 1)){
+			outputMessages.add("You left some candidates without order!");
+			return POST_ORDERING_LANDING_PAGE + committeeId;
+		}
+		
+		// check for duplicates
+		if(!candidateOrders.getCandidatesOrder().stream().mapToInt(o -> o.getCandidateOrder()).allMatch(new HashSet<>()::add)){
+			outputMessages.add("You duplicated some order for candidates");
+			return POST_ORDERING_LANDING_PAGE + committeeId;
+		}
+		
+		// TODO: committee members number could change between GET and POST, but don't handle it
+		// not empty, no duplicates, no null and 0 -> max order should be equal to number of committee members
+		if(committee.getMembers().size() != candidateOrders.getCandidatesOrder().stream().mapToInt(o -> o.getCandidateOrder()).max().getAsInt()){
+			outputMessages.add("You jumped in order of some candidates - order should be continuous");
+			return POST_ORDERING_LANDING_PAGE + committeeId;
+		}
 		
 		committeeCandidateOrderRepository.saveAll(candidateOrders.getCandidatesOrder());
 		
-		return "redirect:/committeepanel";
+		return POST_ORDERING_LANDING_PAGE + committeeId;
 	}
 }
