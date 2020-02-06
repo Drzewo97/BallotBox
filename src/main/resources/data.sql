fc99dc1b7c8943ff5dbaa7ef2544ef52742bd833
Wojciech Drzewiecki <wojciech.drzewiecki@sap.com>
2020-02-06 19:43:57 +0100

    Add admin role to admin user...

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,7 @@ insert into role (id, name) values
 INSERT INTO user (id, username, password, active)
 SELECT '1', 'admin', 'ef51da8f5c6e8f525aacbcf1a09653873c59cac99b09c94e', 1 FROM DUAL
 WHERE NOT EXISTS (SELECT * FROM user);
+
+insert into users_roles (user_id, role_id)
+SELECT '1', '1' FROM DUAL
+WHERE NOT EXISTS (SELECT * FROM users_roles);
