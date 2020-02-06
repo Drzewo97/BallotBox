3ccb1c4aa46d8b16a93e46a618156217bbf6e203
Wojciech Drzewiecki <wojciech.drzewiecki@sap.com>
2020-02-06 18:43:51 +0100

    Change essential data to insert only if table is empty

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,7 @@ insert into role (id, name) values
   ('3', 'ROLE_WARDADMIN'),
   ('4', 'ROLE_COMMITTEEADMIN');
 
-insert into user (id, username, password, active) values
-  ('1', 'admin', 'ef51da8f5c6e8f525aacbcf1a09653873c59cac99b09c94e', 1);
+-- insert only if table is empty
+INSERT INTO user (id, username, password, active)
+SELECT '1', 'admin', 'ef51da8f5c6e8f525aacbcf1a09653873c59cac99b09c94e', '1' FROM DUAL
+WHERE NOT EXISTS (SELECT * FROM user);
